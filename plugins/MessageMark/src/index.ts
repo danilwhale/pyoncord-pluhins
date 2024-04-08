4ea7f00137ddd3e30743cd04498bdb6c2c36c8a0
danil <61111955+danilwhale@users.noreply.github.com>
2024-04-08 13:36:21 +0300

    unhardcode default storage watermark

--- plugins/MessageMark/src/index.ts
+++ plugins/MessageMark/src/index.ts
@@ -1,9 +1,9 @@
 import { storage } from "@vendetta/plugin";
 import { findByProps } from "@vendetta/metro";
-import { insertWatermarkInMessage } from "./utils/formatter";
+import { DEFAULT_FORMAT, insertWatermarkInMessage } from "./utils/formatter";
 import { before, unpatchAll } from "./utils/patching_shit";
 
-if (!storage.watermark) storage.watermark = "Sent from Pyoncord"
+if (!storage.watermark) storage.watermark = DEFAULT_FORMAT
 if (!storage.insertAfter) storage.insertAfter = true
 
 before("sendMessage", findByProps("sendMessage", "receiveMessage"), (args) => insertWatermarkInMessage(args[1]))
